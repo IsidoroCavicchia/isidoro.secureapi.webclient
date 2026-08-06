a5547c8d714b2def8b0176ea4f042a14060ef224
MASTER\t15f56 <isidoro.cavicchia@solidaris.be>
2026-08-06 14:57:12 +0200

    #DOCSERV create design

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal, ChangeDetectionStrategy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ButtonDirective } from 'primeng/button';
+import { SideBarComponent } from '../theme/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonDirective, SideBarComponent],
   templateUrl: './app.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrl: './app.css',
